0ce2abeb99f04a3bfe954ac0f03af1da7cb6b349
Navarroseb <sebastnavarro>
2022-05-23 18:55:41 -0400

    Armando Lógica

---
+++ app.js
@@ -0,0 +1,69 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'images/Fries.jpeg'
+    },
+    {
+        name: 'burger',
+        img: 'images/burger.jpg'
+    },
+    {
+        name: 'Pizza',
+        img: 'images/Pizza.jpg'
+    },
+    {
+        name: 'bentobox',
+        img: 'images/bentobox.jpg'
+    },
+    {
+        name: 'cooffe',
+        img: 'images/cooffe.jpg'
+    },
+    {
+        name: 'lemonpie',
+        img: 'images/lemonpie.png'
+    },
+    {
+        name: 'Sushi',
+        img: 'images/Sushi.jpg'
+    },
+    {
+        name: 'Lobster Roll',
+        img: 'images/Lobster Roll.jpg'
+    },
+    {
+        name: 'fries',
+        img: 'images/Fries.jpeg'
+    },
+    {
+        name: 'burger',
+        img: 'images/burger.jpg'
+    },
+    {
+        name: 'Pizza',
+        img: 'images/Pizza.jpg'
+    },
+    {
+        name: 'bentobox',
+        img: 'images/bentobox.jpg'
+    },
+    {
+        name: 'cooffe',
+        img: 'images/cooffe.jpg'
+    },
+    {
+        name: 'lemonpie',
+        img: 'images/lemonpie.png'
+    },
+    {
+        name: 'Sushi',
+        img: 'images/Sushi.jpg'
+    },
+    {
+        name: 'Lobster Roll',
+        img: 'images/Lobster Roll.jpg'
+    },
+    
+]
+cardArray.sort(() => 0.5 - Math.random())
+console.log(cardArray)
