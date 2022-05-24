5475da5110649ce32059de45f69dd7d8ad7ae562
Navarroseb <sebastnavarro>
2022-05-24 12:53:28 -0400

    modificando imagenes

--- app.js
+++ app.js
@@ -1,78 +1,79 @@
 const cardArray = [
     {
-        name: 'fries',
-        img: 'images/Fries.jpeg'
+        name: 'hml5',
+        img: 'images/hml5.jpg'
     },
     {
-        name: 'burger',
-        img: 'images/burger.jpg'
+        name: 'css3',
+        img: 'images/css3.jpg'
     },
     {
-        name: 'Pizza',
-        img: 'images/Pizza.jpg'
+        name: 'javascript',
+        img: 'images/javascript.png'
     },
     {
-        name: 'bentobox',
-        img: 'images/bentobox.jpg'
+        name: 'react-js',
+        img: 'images/react-js.png'
     },
     {
-        name: 'cooffe',
-        img: 'images/cooffe.jpg'
+        name: 'python',
+        img: 'images/python.png'
     },
     {
-        name: 'lemonpie',
-        img: 'images/lemonpie.png'
+        name: 'flask',
+        img: 'images/flask.png'
     },
     {
-        name: 'Sushi',
-        img: 'images/Sushi.jpg'
+        name: 'sql',
+        img: 'images/sql.jpg'
     },
     {
-        name: 'Lobster Roll',
-        img: 'images/Lobster Roll.jpg'
+        name: 'formik',
+        img: 'images/formik.png'
     },
     {
-        name: 'fries',
-        img: 'images/Fries.jpeg'
+        name: 'hml5',
+        img: 'images/hml5.jpg'
     },
     {
-        name: 'burger',
-        img: 'images/burger.jpg'
+        name: 'css3',
+        img: 'images/css3.jpg'
     },
     {
-        name: 'Pizza',
-        img: 'images/Pizza.jpg'
+        name: 'javascript',
+        img: 'images/javascript.png'
     },
     {
-        name: 'bentobox',
-        img: 'images/bentobox.jpg'
+        name: 'react-js',
+        img: 'images/react-js.png'
     },
     {
-        name: 'cooffe',
-        img: 'images/cooffe.jpg'
+        name: 'python',
+        img: 'images/python.png'
     },
     {
-        name: 'lemonpie',
-        img: 'images/lemonpie.png'
+        name: 'flask',
+        img: 'images/flask.png'
     },
     {
-        name: 'Sushi',
-        img: 'images/Sushi.jpg'
+        name: 'sql',
+        img: 'images/sql.jpg'
     },
     {
-        name: 'Lobster Roll',
-        img: 'images/Lobster Roll.jpg'
+        name: 'formik',
+        img: 'images/formik.png'
     },
     
 ]
 cardArray.sort(() => 0.5 - Math.random())
 
-const gridDisplay = document.querySelector('#grid')
+const gridDisplay = document.querySelector('#grid');
+const cardsChosen = [];
 
 function createboard() {
     for (let i = 0; i < cardArray.length; i++){
         const card = document.createElement('img')
-        card.setAttribute('src', 'images/blank.jpg')
+        card.setAttribute('src', 'images/Fullstack.png')
         card.setAttribute('data-id',i)
         card.addEventListener('click', flipCard)
         gridDisplay.append(card)
@@ -83,8 +84,11 @@ function createboard() {
 createboard()
 
 function flipCard() {
-    const cardId = this.getAttribute('data-id')
-    console.log(cardArray[cardId].name)
-    console.log('click', cardId)
+    console.log(cardArray);
+    const cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardId].name);
+    console.log('click', cardId);
+    console.log(cardsChosen)
+    this.setAttribute('src', cardArray[cardId].img)
 
 }
