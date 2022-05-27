cbec451d81f37e8ecf16a850a8a3a5e777cd11f7
Navarroseb <sebastnavarro>
2022-05-27 11:54:57 -0400

    estableciendo score

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid');
 const cardsChosen = [];
+const cardsChosenIds = []
 
 function createboard() {
     for (let i = 0; i < cardArray.length; i++){
@@ -83,12 +84,29 @@ function createboard() {
 }
 createboard()
 
+function checkMatch(){ 
+
+    const cards = document.querySelectorAll('img')
+
+    console.log('check for match')
+    if(cardsChosen[0] == cardsChosen[1]){
+        alert('Its a Match!')
+        cards[cardsChosenIds[0]].setAttribute('src', 'images/white.png')
+        cards[cardsChosenIds[1]].setAttribute('src', 'images/white.png')
+        cards[cardsChosenIds[0]].removeEventListener('click', flipCard)
+        cards[cardsChosenIds[1]].removeEventListener('click', flipCard)
+
+    }
+}
 function flipCard() {
-    console.log(cardArray);
     const cardId = this.getAttribute('data-id');
     cardsChosen.push(cardArray[cardId].name);
-    console.log('click', cardId);
+    cardsChosenIds.push(cardId)
     console.log(cardsChosen)
+    console.log(cardsChosenIds)
     this.setAttribute('src', cardArray[cardId].img)
+    if(cardsChosen.length === 2){
+        setTimeout( checkMatch, 500)
+    }
 
 }
